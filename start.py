052b4b840bfef2686e8f578293a00677130b6cb2
Slavo Heys <ilodz24hd@gmail.com>
2023-04-29 19:39:26 +0200

    Program mini baza danych przy wykorzystaniu biblioteki pandas #3

--- start.py
+++ start.py
@@ -16,10 +16,12 @@ try:
     # usuń kolumnę 'Unnamed: 0'
     df = df.drop('Unnamed: 0', axis=1)
 except FileNotFoundError:
-    df = pd.DataFrame(columns=['data', 'imie', 'nazwisko', 'pesel', 'wiek', 'plec', 'miasto', 'kod', 'ulica', 'nr_budynku',
-                               'nr_mieszkania', 'tel_stacjonarny', 'tel_komórkowy', 'email', 'dodatkowe_informacje'])
+    df = pd.DataFrame(
+        columns=['data', 'imie', 'nazwisko', 'pesel', 'wiek', 'plec', 'miasto', 'kod', 'ulica', 'nr_budynku',
+                 'nr_mieszkania', 'tel_stacjonarny', 'tel_komórkowy', 'email', 'dodatkowe_informacje'])
 
 
+# --------------------------------------------------------------------------------
 # Program główny
 class Program:
     def __init__(self):
@@ -67,7 +69,7 @@ class Program:
         print('-' * 50)
         # dzisejsza data i godzina
         self.data = time.strftime('%Y-%m-%d %H:%M')
-# --------------------------------------------------------------------------------
+        # --------------------------------------------------------------------------------
         self.imie = input('Podaj imię: ')
         if self.imie != '':
             # sprawdz czy imię jest poprawne
@@ -87,7 +89,7 @@ class Program:
                 self.dodaj()
             # pierwsza litera nazwiska z dużej litery
             self.nazwisko = self.nazwisko.capitalize()
-# --------------------------------------------------------------------------------
+        # --------------------------------------------------------------------------------
         self.pesel = input('Podaj lub PESEL: ')
         if self.pesel != '':
             # sprawdz PESEL jest poprawny
@@ -135,7 +137,7 @@ class Program:
             self.pesel = ''
             self.wiek = ''
             self.plec = ''
-# --------------------------------------------------------------------------------
+        # --------------------------------------------------------------------------------
         self.miasto = input('Podaj miasto: ')
         if self.miasto != '':
             # pierwsza litera miasta z dużej litery
@@ -152,13 +154,13 @@ class Program:
                 self.dodaj()
         else:
             self.kod = ''
-# --------------------------------------------------------------------------------
+        # --------------------------------------------------------------------------------
         self.ulica = input('Podaj ulicę: ')
-# --------------------------------------------------------------------------------
+        # --------------------------------------------------------------------------------
         self.nr_budynku = input('Podaj numer budynku: ')
-# --------------------------------------------------------------------------------
+        # --------------------------------------------------------------------------------
         self.nr_mieszkania = input('Podaj numer mieszkania: ')
-# --------------------------------------------------------------------------------
+        # --------------------------------------------------------------------------------
         self.tel_stacjonarny = input('Podaj numer telefonu stacjonarnego: ')
         # sprawdz czy numer telefonu stacjonarnego jest poprawny
         if self.tel_stacjonarny != '':
@@ -168,7 +170,7 @@ class Program:
                 self.dodaj()
         else:
             self.tel_stacjonarny = ''
-# --------------------------------------------------------------------------------
+        # --------------------------------------------------------------------------------
         self.tel_komorkowy = input('Podaj numer telefonu komórkowego: ')
         if self.tel_komorkowy != '':
             # sprawdz czy numer telefonu komórkowego jest poprawny
@@ -185,7 +187,7 @@ class Program:
             self.tel_komorkowy = self.tel_komorkowy[:3] + '-' + self.tel_komorkowy[3:6] + '-' + self.tel_komorkowy[6:]
         else:
             self.tel_komorkowy = ''
-# --------------------------------------------------------------------------------
+        # --------------------------------------------------------------------------------
         self.email = input('Podaj adres email: ')
         if self.email != '':
             # sprawdz czy adres email jest poprawny
@@ -195,18 +197,19 @@ class Program:
                 self.dodaj()
         else:
             self.email = ''
-# --------------------------------------------------------------------------------
+        # --------------------------------------------------------------------------------
         self.dodatkowe_informacje = input('Podaj dodatkowe informacje: ')
-# --------------------------------------------------------------------------------
+        # --------------------------------------------------------------------------------
         # dodaj rekord do DataFrame
-        df.loc[len(df)] = [self.data, self.imie, self.nazwisko, self.pesel, self.wiek, self.plec, self.miasto, self.kod, self.ulica,
+        df.loc[len(df)] = [self.data, self.imie, self.nazwisko, self.pesel, self.wiek, self.plec, self.miasto, self.kod,
+                           self.ulica,
                            self.nr_budynku, self.nr_mieszkania, self.tel_stacjonarny, self.tel_komorkowy, self.email,
                            self.dodatkowe_informacje]
 
         # zapisz DataFrame do pliku
         df.to_csv('baza_danych.csv')
 
-        #sprawdz czy rekord został dodany
+        # sprawdz czy rekord został dodany
         if self.imie in df.values:
             print('Rekord został dodany!')
             time.sleep(2)
@@ -221,13 +224,15 @@ class Program:
             self.dodaj()
         else:
             self.menu()
-# --------------------------------------------------------------------------------
+
+    # --------------------------------------------------------------------------------
     def wyswietl(self):
         os.system('cls')
         print('Wyświetlanie wszystkich rekordów')
         print('\n')
         data = []
-        self.header = ['data zapisu', 'imie', 'nazwisko', 'pesel','wiek', 'pleć', 'miasto', 'kod', 'ulica', 'nr_budynku',
+        self.header = ['data zapisu', 'imie', 'nazwisko', 'pesel', 'wiek', 'pleć', 'miasto', 'kod', 'ulica',
+                       'nr_budynku',
                        'nr_mieszkania', 'tel_stacjonarny', 'tel_komórkowy', 'email', 'dodatkowe_informacje']
         # wyświetl DataFrame w tabeli texttable
         for i in range(len(df)):
@@ -241,12 +246,73 @@ class Program:
         input('Naciśnij ENTER aby wrócić do menu.')
         self.menu()
 
-
+    # --------------------------------------------------------------------------------
     def wyszukaj(self):
-        pass
+        os.system('cls')
+        print('Wyszukiwanie rekordów')
+        print('\n')
+        self.wyszukiwanie = input('Podaj dane do wyszukania (imie, nazwisko, pesel, tel komórkowy, email: ')
+        # sprawdz czy podano dane do wyszukania
+        if self.wyszukiwanie == '':
+            print('Nie podano danych do wyszukania!')
+            time.sleep(2)
+            self.wyszukaj()
+        else:
+            # sprawdź czy podano imię
+            if self.wyszukiwanie in df['imie'].values:
+                print('Wyniki wyszukiwania dla imienia: ' + self.wyszukiwanie)
+                print('\n')
+                # wyszukaj imię
+                for i in range(len(df)):
+                    if df.loc[i][1] == self.wyszukiwanie:
+                        print(df.loc[i])
+                        print('\n')
+            # sprawdź czy podano nazwisko
+            elif self.wyszukiwanie in df['nazwisko'].values:
+                print('Wyniki wyszukiwania dla nazwiska: ' + self.wyszukiwanie)
+                print('\n')
+                # wyszukaj nazwisko
+                for i in range(len(df)):
+                    if df.loc[i][2] == self.wyszukiwanie:
+                        print(df.loc[i])
+                        print('\n')
+            # sprawdź czy podano pesel
+            elif self.wyszukiwanie in df['pesel'].values:
+                print('Wyniki wyszukiwania dla peselu: ' + self.wyszukiwanie)
+                print('\n')
+                # wyszukaj pesel
+                for i in range(len(df)):
+                    if df.loc[i][3] == self.wyszukiwanie:
+                        print(df.loc[i])
+                        print('\n')
+            # sprawdź czy podano numer telefonu komórkowego
+            elif self.wyszukiwanie in df['tel_komorkowy'].values:
+                print('Wyniki wyszukiwania dla numeru telefonu komórkowego: ' + self.wyszukiwanie)
+                print('\n')
+                # wyszukaj numer telefonu komórkowego
+                for i in range(len(df)):
+                    if df.loc[i][12] == self.wyszukiwanie:
+                        print(df.loc[i])
+                        print('\n')
+            # sprawdź czy podano adres email
+            elif self.wyszukiwanie in df['email'].values:
+                print('Wyniki wyszukiwania dla adresu email: ' + self.wyszukiwanie)
+                print('\n')
+                # wyszukaj adres email
+                for i in range(len(df)):
+                    if df.loc[i][13] == self.wyszukiwanie:
+                        print(df.loc[i])
+                        print('\n')
+            else:
+                print('Nie znaleziono rekordów!')
+                time.sleep(2)
+                self.menu()
+        input('Naciśnij ENTER aby wrócić do menu.')
 
+    # --------------------------------------------------------------------------------
     def usun(self):
         pass
+
 
 # start programu
 Program()
